6279e18b64f7fdeb97012cbaea2d2ef95de2ad91
Daniela Postigo <dani.postigo@gmail.com>
2017-06-11 18:35:15 -0400

    updates 3

---
+++ Sources/Foundation.swift
@@ -0,0 +1,25 @@
+//
+// Created by Dani Postigo on 4/8/17.
+//
+
+import Foundation
+
+extension RangeReplaceableCollection {
+    func appending(_ item: Self.Iterator.Element) -> Self {
+        var ret = Array(self) as! Self
+        ret.append(item)
+        return ret
+    }
+}
+
+extension Sequence {
+    public func select(_ find: (Self.Iterator.Element) -> Bool) -> Self.Iterator.Element? {
+        var generator = self.makeIterator()
+        while let elem = generator.next() {
+            if(find(elem)) {
+                return elem
+            }
+        }
+        return nil
+    }
+}/Users/dpostigo/vapor/andea/Sources/App/Foundation.swift
