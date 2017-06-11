2ea6feab69991849e2851f53d3f7e4d4b1f62b14
Daniela Postigo <dani.postigo@gmail.com>
2017-06-11 18:33:00 -0400

    updates 2

--- Package.swift
+++ Package.swift
@@ -1,9 +1,6 @@
 import PackageDescription
 
 let package = Package(
-    name: "andea",
-    dependencies: [
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2, minor: 0)
-    ]
+    name: "Andea"
 )
 
