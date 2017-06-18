cfe36370ceb162dc6bf2fb5a63b0202fc39bb6b6
Daniela Postigo <dani.postigo@gmail.com>
2017-06-18 19:03:42 -0400

    things

---
+++ Sources/Andea/Andea.swift
@@ -0,0 +1,11 @@
+//
+// Created by Dani Postigo on 11/24/16.
+//
+
+import Foundation
+
+public typealias JSON = [String: Any]
+public typealias Block = (Void) -> Void
+public typealias Completion = (Void) -> Void
+public typealias Failure = (NSError) -> Void
+
--- Sources/Andea/main.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-//let drop = Droplet()
-//
-//drop.get { request in request.description }
-//
-//drop.run()
