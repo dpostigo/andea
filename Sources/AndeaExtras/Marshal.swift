0852e45cf9dae7c02fc30e5308a3fca661155edb
Daniela Postigo <dani.postigo@gmail.com>
2017-06-29 11:46:54 -0400

    wip

--- Sources/AndeaExtras/Marshal.swift
+++ Sources/AndeaExtras/Marshal.swift
@@ -36,17 +36,41 @@ extension MarshaledObject {
     public func optionalDouble(for key: KeyType) -> Double? {
         return (try? self.double(for: key)) ?? nil
     }
-    public func double(for key: KeyType) throws -> Double {
-        let value = try self.any(for: key)
-        guard let string = value as? String else { throw MarshalError.typeMismatch(expected: String.self, actual: type(of: value)) }
-        guard let double = Double(string) else { throw MarshalError.typeMismatch(expected: Double.self, actual: type(of: string)) }
-        return double
+
+    public func double<A: ValueType>(for key: KeyType) throws -> A {
+        let any = try self.any(for: key)
+        do {
+            guard let result = try A.double(from: any) as? A else {
+                throw MarshalError.typeMismatchWithKey(key: key.stringValue, expected: A.self, actual: type(of: any))
+            }
+            return result
+        }
+        catch let MarshalError.typeMismatch(expected: expected, actual: actual) {
+            throw MarshalError.typeMismatchWithKey(key: key.stringValue, expected: expected, actual: actual)
+        }
     }
+
 
     public func unixDate(for key: KeyType) throws -> Date {
         let value = try self.any(for: key)
-        return try Date.unixDate(from: value)
+        do {
+            return try Date.unixDate(from: value)
+        } catch  {
+
+            if let error = error as? MarshalError {
+                switch error {
+                    case let .typeMismatch(expected, actual):
+                        throw MarshalError.typeMismatchWithKey(key: key, expected: expected, actual: actual)
+                    default: throw error
+                }
+            } else {
+                throw error
+            }
+           
+        }
+
     }
+
 }
 
 extension ValueType {
@@ -57,12 +81,14 @@ extension ValueType {
     }
 
     public static func double(from object: Any) throws -> Double {
-        guard let value = object as? String else { throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object)) }
-        guard let double = Double(value) else { throw MarshalError.typeMismatch(expected: Double.self, actual: type(of: object)) }
-        return double
+        if let value = object as? NSNumber {
+            return value.doubleValue
+        } else {
+            guard let value = object as? String else { throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object)) }
+            guard let double = Double(value) else { throw MarshalError.typeMismatch(expected: Double.self, actual: type(of: object)) }
+            return double
+        }
     }
-
-
 }
 
 
@@ -70,8 +96,13 @@ extension Date : ValueType {
 
     static let unixFormatter = DateFormatter(dateFormat: "yyyy-MM-dd HH:mm:ss")
 
+    public var unixString: String {
+        return Date.unixFormatter.string(from: self)
+    }
+
     public static func unixDate(from object: Any) throws -> Date {
-        guard let string = object as? String else {  throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object)) }
+        guard let string = object as? String else {
+            throw MarshalError.typeMismatch(expected: String.self, actual: type(of: object)) }
         guard let date = Date.unixFormatter.date(from: string) else { throw MarshalError.typeMismatch(expected: "UNIX date string", actual: string) }
         return date
     }
