222293fe71743bb6d2352ccb1595130183f2d2ab
Daniela Postigo <dani.postigo@gmail.com>
2017-08-17 21:58:40 -0400

    wip

--- Sources/Andea-NSApplication/NSBinding.swift
+++ Sources/Andea-NSApplication/NSBinding.swift
@@ -8,6 +8,7 @@
 import Cocoa
 
 extension NSBindingName {
+    public static let selection: NSBindingName = NSBindingName(rawValue: "selection")
     public static let selectionIndex: NSBindingName = NSBindingName(rawValue: "selectionIndex")
     public static let enabled2: NSBindingName = NSBindingName(rawValue: "enabled2")
 
