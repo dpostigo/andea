89030decb10d82156b6a959a2a5c31668d6c7f1f
Daniela Postigo <dani.postigo@gmail.com>
2018-03-08 15:41:51 -0500

    unwrappers

--- Sources/Foundation/Array.swift
+++ Sources/Foundation/Array.swift
@@ -11,4 +11,33 @@ extension Array {
 	}
 }
 
+extension Array where Element: Equatable {
+	
+	public mutating func insert(_ element: Element, after: Element) {
+		if let index = self.index(of: after) {
+			self.insert(element, at: self.index(after: index))
+		}
+	}
+}
 
+
+extension Collection {
+	
+	public func first(handler: (Element) -> Void) {
+		if let first = self.first { handler(first) }
+	}
+	
+	public func isEmpty(handler: () -> Void) {
+		if self.isEmpty { handler() }
+	}
+}
+
+
+extension Optional {
+	public func unwrapped(handler: (Wrapped) -> Void) {
+		switch self {
+			case .none: break
+			case .some(let wrapped): handler(wrapped)
+		}
+	}
+}
