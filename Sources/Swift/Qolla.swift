d66704c76f36256cf79009bf3d9c5ea6ed984969
Daniela Postigo <dani.postigo@gmail.com>
2018-03-11 18:29:00 -0400

    titled protocol

--- Sources/Swift/Qolla.swift
+++ Sources/Swift/Qolla.swift
@@ -4,3 +4,23 @@
 
 @_exported import Qolla
 
+
+public protocol Titled {
+    var title: String { get }
+}
+
+extension Sequence where Element: Titled {
+    public var titles: [String] { return self.map { $0.title } }
+}
+
+extension RawRepresentable where RawValue == Int, Self: Titled {
+    
+    public init?(title: String) {
+        switch Self.titles.index(of: title) {
+            case .none: return nil
+            case .some(let index): self.init(rawValue: index)
+        }
+    }
+    
+    public static var titles: [String] { return self.all.titles }
+}
