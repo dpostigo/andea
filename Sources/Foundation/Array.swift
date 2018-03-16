8e4936c51f35172356ad02f8a7d35ecaa65c19a7
Daniela Postigo <dani.postigo@gmail.com>
2018-03-16 12:17:06 -0400

    foundation utils

--- Sources/Foundation/Array.swift
+++ Sources/Foundation/Array.swift
@@ -25,17 +25,20 @@ extension Array where Element: Equatable {
 		return self[next]
 	}
 }
+extension Sequence where Element: Hashable {
+	public var unique: [Element] {
+		return Set<Element>(self).array
+	}
+}
+
 
 extension Sequence {
 	public var array: [Element] {
 		return Array(self)
 	}
-}
-
-extension Sequence where Element: Hashable {
-    public var unique: [Element] {
-        return Set<Element>(self).array
-    }
+	public func all(_ body: (Self.Element) throws -> Bool) rethrows -> Bool {
+		return try self.filter { try body($0) == false }.count > 0
+	}
 }
 
 
