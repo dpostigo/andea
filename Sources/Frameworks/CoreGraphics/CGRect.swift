c7a1804f303b299e7958ec8ea6eaf116403a625e
Daniela Postigo <dani.postigo@gmail.com>
2018-03-26 16:47:21 -0400

    coregraphics

--- Sources/Frameworks/CoreGraphics/CGRect.swift
+++ Sources/Frameworks/CoreGraphics/CGRect.swift
@@ -8,24 +8,6 @@
 
 import Foundation
 
-extension CGRect {
-    public init(width: CGFloat) {
-        self.init(x: 0, y: 0, width: width, height: width)
-    }
-
-    public init(width: CGFloat, height: CGFloat) {
-        self.init(x: 0, y: 0, width: width, height: height)
-    }
-
-    public init(size: CGSize) {
-        self.init(origin: CGPoint.zero, size: size)
-    }
-
-    public init(origin: CGPoint, width: CGFloat, height: CGFloat) {
-        self.init(x: origin.x, y: origin.y, width: width, height: height)
-    }
-}
-
 extension CGRect: ExpressibleByArrayLiteral {
     public typealias ArrayLiteralElement = CGFloat
 
--- Sources/Frameworks/CoreGraphics/CGSize.swift
+++ Sources/Frameworks/CoreGraphics/CGSize.swift
@@ -4,6 +4,33 @@
 
 import Foundation
 
+extension CGSize: ExpressibleByArrayLiteral {
+	public typealias ArrayLiteralElement = CGFloat
+	
+	public init(arrayLiteral elements: ArrayLiteralElement...) {
+		self.init(width: elements.first!, height: elements.last!)
+	}
+	
+	// MARK: Operators
+	
+	public static func *=(lhs: inout CGSize, right: CGFloat) { lhs = lhs * right }
+	
+	public static func -(lhs: CGSize, rhs: CGSize) -> CGSize { return [lhs.width - rhs.width, lhs.height - rhs.height] }
+	public static func +(lhs: CGSize, rhs: CGSize) -> CGSize { return [lhs.width + rhs.width, lhs.height + rhs.height] }
+	
+	public static func +(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width + rhs, lhs.height + rhs] }
+	public static func -(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width - rhs, lhs.height - rhs] }
+	public static func *(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width * rhs, lhs.height * rhs] }
+	public static func /(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width / rhs, lhs.height / rhs] }
+	
+	public static func +(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width + CGFloat(rhs), lhs.height + CGFloat(rhs)] }
+	public static func -(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width - CGFloat(rhs), lhs.height - CGFloat(rhs)] }
+	public static func *(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width * CGFloat(rhs), lhs.height * CGFloat(rhs)] }
+	public static func /(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width / CGFloat(rhs), lhs.height / CGFloat(rhs)] }
+	
+}
+
+
 extension CGSize {
 	
 	// MARK: Getters
@@ -18,7 +45,7 @@ extension CGSize {
 		return self.applying(CGAffineTransform(scaleX: valueX, y: valueY))
 	}
 
-	public func scale(to: CGSize) -> CGFloat {
+	public func ratio(to: CGSize) -> CGFloat {
 		return self.ratio > to.ratio ? to.width / self.width : to.height / self.height
 	}
 	
@@ -28,26 +55,6 @@ extension CGSize {
 		self.init(width: value, height: value)
 	}
 	
-	// MARK: Operators
-	
-	public static func *=(lhs: inout CGSize, right: CGFloat) {
-		lhs.width *= right
-		lhs.height *= right
-	}
-	
-	public static func -(lhs: CGSize, rhs: CGSize) -> CGSize { return [lhs.width - rhs.width, lhs.height - rhs.height] }
-	public static func +(lhs: CGSize, rhs: CGSize) -> CGSize { return [lhs.width + rhs.width, lhs.height + rhs.height] }
-	
-	public static func +(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width + rhs, lhs.height + rhs] }
-	public static func -(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width - rhs, lhs.height - rhs] }
-	public static func *(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width * rhs, lhs.height * rhs] }
-	public static func /(lhs: CGSize, rhs: CGFloat) -> CGSize { return [lhs.width / rhs, lhs.height / rhs] }
-	
-	public static func +(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width + CGFloat(rhs), lhs.height + CGFloat(rhs)] }
-	public static func -(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width - CGFloat(rhs), lhs.height - CGFloat(rhs)] }
-	public static func *(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width * CGFloat(rhs), lhs.height * CGFloat(rhs)] }
-	public static func /(lhs: CGSize, rhs: Double) -> CGSize { return [lhs.width / CGFloat(rhs), lhs.height / CGFloat(rhs)] }
-
 #if os(iOS)
 	public static var compressed: CGSize {
 		return UILayoutFittingCompressedSize
@@ -56,11 +63,3 @@ extension CGSize {
 
 }
 
-
-extension CGSize: ExpressibleByArrayLiteral {
-	public typealias ArrayLiteralElement = CGFloat
-	
-	public init(arrayLiteral elements: ArrayLiteralElement...) {
-		self.init(width: elements.first!, height: elements.last!)
-	}
-}
